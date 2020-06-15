7faf3fdfdb1f4e647d0c84930073fa9155acefe5
RickBraunn <ricardoalmeidabraunn@gmail.com>
2020-06-15 16:44:41 -0300

    Banco de Dados adicionado
    alterado as config do BD

--- configuracao.php
+++ configuracao.php
@@ -3,13 +3,12 @@
 const VIEWPATH = __DIR__ . '/pages';
 const ROOT = __DIR__;
 
-const DB_NAME = 'meuprimeiroprojeto';
+const DB_NAME = 'tcc';
 const DB_HOST = 'localhost';
 const DB_USER = 'root';
-const DB_PASSWORD = '';
+const DB_PASSWORD = 'root';
 
 
 function criptografa($senha){
     return sha1($senha);
 }
-  
