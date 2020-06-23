5a4996cbd530037e2aa00da1303485691a2bd11c
RickBraunn <ricardoalmeidabraunn@gmail.com>
2020-06-23 15:03:49 -0300

    Cadastro Advogados
    Adicionado o cadastro de advogados

--- App/Controllers/advogado.php
+++ App/Controllers/advogado.php
@@ -7,7 +7,7 @@ use App\Controller;
 use App\Conexao;
 use App\Bootgrid;
 
-class Categoria Extends Controller
+class Advogado Extends Controller
 {
     public function index(){
 
@@ -16,20 +16,20 @@ class Categoria Extends Controller
         $db = Conexao::connect();
 
 
-        echo $this->template->twig->render('categoria/listagem.html.twig');
+        echo $this->template->twig->render('advogado/listagem.html.twig');
 
     }
 
     public function formCadastrar()
     {
-        echo $this->template->twig->render('categoria/cadastrar.html.twig');
+        echo $this->template->twig->render('advogado/cadastrar.html.twig');
     }
 
     public function formEditar($id)
     {
         $db = Conexao::connect();
 
-        $sql = "SELECT * FROM categoria WHERE id=:id";
+        $sql = "SELECT * FROM advogado WHERE id=:id";
 
         $query = $db->prepare($sql);
         $query->bindParam(":id", $id);
@@ -37,16 +37,24 @@ class Categoria Extends Controller
 
         $linha = $query->fetch();
 
-        echo $this->template->twig->render('categoria/editar.html.twig', compact('linha'));
+        echo $this->template->twig->render('advogado/editar.html.twig', compact('linha'));
     }
 
     public function salvarCadastrar(){
         $db = Conexao::connect();
 
-        $sql = "INSERT INTO categoria (nome) VALUES (:nome)";
+        $sql = "INSERT INTO advogado (nome_adv, sobrenome_adv, email_adv, estados_adv, Cidades_idCidades, telefone_adv, nome_usuario_adv, senha_adv, formacao  ) VALUES (:nome_adv, :sobrenome_adv, :email_adv, :estados_adv, :Cidades_idCidades, :telefone_adv, :nome_usuario_adv, :senha_adv, :formacao)";
 
         $query = $db->prepare($sql);
-        $query->bindParam(":nome", $_POST['nome']);
+        $query->bindParam(":nome_adv", $_POST['nome_adv']);
+        $query->bindParam(":sobrenome_adv", $_POST['sobrenome_adv']);
+        $query->bindParam(":email_adv", $_POST['email_adv']);
+        $query->bindParam(":estados_adv", $_POST['estados_adv']);
+        $query->bindParam(":Cidades_idCidades", $_POST['Cidades_idCidades']);
+        $query->bindParam(":telefone_adv", $_POST['telefone_adv']);
+        $query->bindParam(":nome_usuario_adv", $_POST['nome_usuario_adv']);
+        $query->bindParam(":senha_adv", $_POST['senha_adv']);
+        $query->bindParam(":formacao", $_POST['formacao']);
         $query->execute();
 
         if ($query->rowCount()==1) {
@@ -63,7 +71,7 @@ class Categoria Extends Controller
     public function salvarEditar(){
         $db = Conexao::connect();
 
-        $sql = "UPDATE categoria SET nome=:nome WHERE id=:id";
+        $sql = "UPDATE advogado SET nome=:nome WHERE id=:id";
 
         $query = $db->prepare($sql);
         $query->bindParam(":nome", $_POST['nome']);
@@ -85,7 +93,7 @@ class Categoria Extends Controller
     {
         $db = Conexao::connect();
 
-        $sql = "DELETE FROM categoria WHERE id=:id";
+        $sql = "DELETE FROM advogado WHERE id=:id";
 
         $query = $db->prepare($sql);
         $query->bindParam(":id", $_POST['id']);
@@ -105,11 +113,11 @@ class Categoria Extends Controller
     public function bootgrid()
     {
         $busca = addslashes($_POST['searchPhrase']);
-        $sql = "SELECT `id`, `nome` FROM categoria WHERE 1 ";
+        $sql = "SELECT `id`, `nome` FROM advogado WHERE 1 ";
 
         if ($busca!=''){
             $sql .= " and (
-                        nome LIKE '%{$busca}%' 
+                        nome LIKE '%{$busca}%'
                         ) ";
         }
 
