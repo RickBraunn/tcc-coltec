090f83d3c11be91830f29e9bac1dd3617300e726
RickBraunn <ricardoalmeidabraunn@gmail.com>
2020-07-27 15:20:15 -0300

    Segurança Advogado
    *Adicionanado segurança nas paginas do advogado
    *Adicionado Controller OAB

--- advogado/App/Controllers/cliente.php
+++ advogado/App/Controllers/cliente.php
@@ -6,8 +6,10 @@ namespace App\Controllers;
 use App\Controller;
 use App\Conexao;
 use App\Bootgrid;
+use App\ControllerSeguro;
 
-class Cliente Extends Controller
+
+class Cliente Extends ControllerSeguro
 {
     public function index()
     {
