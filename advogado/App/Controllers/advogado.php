fb582458544b628dd7534c5a9ece74e376a8e640
RickBraunn <ricardoalmeidabraunn@gmail.com>
2020-08-26 17:03:32 -0300

    Cadastro Publico Advogado
    *Cadastrar advogado Publico

--- advogado/App/Controllers/advogado.php
+++ advogado/App/Controllers/advogado.php
@@ -17,16 +17,7 @@ class Advogado Extends ControllerSeguro
         echo $this->template->twig->render('advogado/menu.html.twig');
     }
 
-    public function formCadastrar()
-    {
-        $db = Conexao::connect();
-
-        $sql = "SELECT * FROM cidade ORDER BY nome";
-        $resultados = $db->query($sql);
-        $cidades = $resultados->fetchALl();
-
-        echo $this->template->twig->render('advogado/cadastrar.html.twig', compact('cidades'));
-    }
+    
 
     public function formEditar($id_adv)
     {
@@ -47,32 +38,7 @@ class Advogado Extends ControllerSeguro
         echo $this->template->twig->render('advogado/editar.html.twig', compact('linha', 'cidades'));
     }
 
-    public function salvarCadastrar(){
-        $db = Conexao::connect();
-
-        $sql = "INSERT INTO advogado (nome_adv, sobrenome_adv, email_adv, cidade_adv, telefone_adv, nome_usuario_adv, senha_adv, formacao  ) VALUES (:nome_adv, :sobrenome_adv, :email_adv, :cidade_adv, :telefone_adv, :nome_usuario_adv, :senha_adv, :formacao)";
-
-        $query = $db->prepare($sql);
-        $query->bindParam(":nome_adv", $_POST['nome_adv']);
-        $query->bindParam(":sobrenome_adv", $_POST['sobrenome_adv']);
-        $query->bindParam(":email_adv", $_POST['email_adv']);
-        $query->bindParam(":cidade_adv", $_POST['cidade_adv']);
-        $query->bindParam(":telefone_adv", $_POST['telefone_adv']);
-        $query->bindParam(":nome_usuario_adv", $_POST['nome_usuario_adv']);
-        $query->bindParam(":senha_adv", $_POST['senha_adv']);
-        $query->bindParam(":formacao", $_POST['formacao']);
-        $query->execute();
-
-        if ($query->rowCount()==1) {
-            $retorno['status'] = 1;
-            $retorno['mensagem'] = 'Advogado cadastrado com sucesso';
-        }else{
-            $retorno['status'] = 0;
-            $retorno['mensagem'] = 'Erro ao inserir os dados';
-        }
-
-        echo $this->jsonResponse($retorno);
-    }
+   
 
     public function salvarEditar(){
         $db = Conexao::connect();
