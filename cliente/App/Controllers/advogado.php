c75fbe2a2584b66f036a0995f23de874c032a6d1
RickBraunn <ricardoalmeidabraunn@gmail.com>
2020-09-30 14:31:24 -0300

    Lista Advogados para Clientes
    Vai dai Prof

--- cliente/App/Controllers/advogado.php
+++ cliente/App/Controllers/advogado.php
@@ -20,7 +20,35 @@ class Advogado Extends Controller
 
     }
 
+    public function formcadastrar()
+    {
 
+        $db = Conexao::connect();
+
+
+        $sql = "SELECT     
+     concat(advogado.nome_adv, ' ', advogado.sobrenome_adv) as nome_adv,
+    advogado.formacao,
+    advogado.foto,
+    concat(oab.numero_oab,'/', estado.sigla_estado ) as numero_oab
+     FROM advogado Inner Join
+    oab On oab.id_adv = advogado.id_adv Inner Join
+    estado On oab.estados_oab = estado.id_estado Inner Join
+    area_adv On area_adv.id_adv = advogado.id_adv";
+        $resultados = $db->query($sql);
+        $adv = $resultados->fetchALl();
+
+        $sql = "SELECT
+    area.nome_area
+From
+    area Inner Join
+    area_adv On area_adv.id_area = area.id_area Inner Join
+    advogado On area_adv.id_adv = advogado.id_adv";
+        $resultados = $db->query($sql);
+        $area = $resultados->fetchALl();
+
+        echo $this->template->twig->render('advogado/cadastrar.html.twig', compact("adv", "area"));
+    }
 
     public function bootgrid()
     {
