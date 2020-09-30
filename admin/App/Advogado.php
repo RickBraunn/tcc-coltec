300463ba30eef5c69b0a2d8413eb7adf19e0ebe8
RickBraunn <ricardoalmeidabraunn@gmail.com>
2020-09-30 15:43:20 -0300

    Update Advogado.php
    Co-Authored-By: profricardovergutz <44248328+profricardovergutz@users.noreply.github.com>

--- admin/App/Advogado.php
+++ admin/App/Advogado.php
@@ -10,7 +10,6 @@ class Advogado
     static public function verificaCadastro($id_adv)
     {
         /*
-         * conecta no BD
          * consulta SELECT * FROM area_adv WHERE area_adv.id_adv=:id_adv;
          * consulta SELECT * FROM oab WHERE oab.id_adv=:id_adv AND oab.status_oab='Aprovado';
          *         if (OK os 2){
@@ -20,7 +19,44 @@ class Advogado
          *          }
          *          UPDATE advogado SET cadastro_completo=:cadastro_completo WHERE id_adv=:id_adv
          */
+        $db = Conexao::connect();
 
+        $sql = "SELECT * FROM area_adv WHERE area_adv.id_adv=:id_adv";
+            $query = $db->prepare($sql);
+            $query->bindParam(":id_adv", $id_adv);
+            $query->execute();
+        if ($query->rowCount() == 0)
+        {
+            $cadastro_completo = 0;
+            $sql = "UPDATE advogado SET cadastro_completo=:cadastro_completo WHERE id_adv=:id_adv";
+                $query = $db->prepare($sql);
+                $query->bindParam(":id_adv", $id_adv);
+                $query->bindParam(":cadastro_completo", $cadastro_completo);
+                $query->execute();
+
+        }else{
+            $sql = "SELECT * FROM oab WHERE oab.id_adv=:id_adv AND oab.status_oab='Aprovado'";
+                $query = $db->prepare($sql);
+                $query->bindParam(":id_adv", $id_adv);
+                $query->execute();
+            if ($query->rowCount() == 0)
+            {
+                $sql = "UPDATE advogado SET cadastro_completo=:cadastro_completo WHERE id_adv=:id_adv";
+                    $query = $db->prepare($sql);
+                    $query->bindParam(":id_adv", $id_adv);
+                    $query->bindParam(":cadastro_completo", $cadastro_completo);
+                    $query->execute();
+            }else{
+                $cadastro_completo = 1;
+                $sql = "UPDATE advogado SET cadastro_completo=:cadastro_completo WHERE id_adv=:id_adv";
+                    $query = $db->prepare($sql);
+                    $query->bindParam(":id_adv", $id_adv);
+                    $query->bindParam(":cadastro_completo", $cadastro_completo);
+                    $query->execute();
+                
+            }
+
+        }
     }
 
 }
