bcede828d426008519a9c8ec3ab10e3c97daea05
RickBraunn <ricardoalmeidabraunn@gmail.com>
2020-10-28 10:40:33 -0300

    Arrumado Configuracao

--- advogado/configuracao.php
+++ advogado/configuracao.php
@@ -3,10 +3,10 @@
 const VIEWPATH = __DIR__ . '/pages';
 const ROOT = __DIR__;
 
-const DB_NAME = 'tcc-ricardo';
+const DB_NAME = 'tcc';
 const DB_HOST = 'localhost';
 const DB_USER = 'root';
-const DB_PASSWORD = '';
+const DB_PASSWORD = 'root';
 const DIR_SOLICITACAO = __DIR__ . '/../files/solicitacoes/';
 
 function criptografa($senha){
