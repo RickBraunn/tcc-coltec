ec82d6d892d1ea2bce3b1977a43a2a51477828c0
RickBraunn <ricardoalmeidabraunn@gmail.com>
2020-11-04 17:23:17 -0300

    Corrigidos classe notificacao

--- advogado/App/Controllers/notificacao.php
+++ advogado/App/Controllers/notificacao.php
@@ -8,19 +8,25 @@ use App\ControllerSeguro;
 class Notificacao extends ControllerSeguro
 {
 
-    public function notifica($id_user, $tipo_user){
-        $query =$this->db->prepare('SELECT * FROM notifica Where id_user=:id_user and tipo_user=:tipo_user');
+    public function notifica(){
+         $id_user=$_SESSION['id_adv'];
+         $tipo_user = "adv";
+        $query =$this->db->prepare('SELECT * FROM notificacao Where id_user=:id_user and tipo_user=:tipo_user and data_leitura is null ');
         $query->bindParam(":id_user", $id_user);
         $query->bindParam(":tipo_user", $tipo_user);
         $query->execute();
+        $this->jsonResponse($query->fetchAll());
      }
 
-     public function inserir($id_user, $tipo_user, $texto){
-        $query = $this->db->prepare("INSERT INTO notificacao (id_user, tipo_user, texto) values (:id_user, :tipo_user, :texto)");
-        $query->bindParam(":id_user", $id_user);
-        $query->bindParam(":tipo_user", $tipo_user);
-        $query->bindParam(":texto", $texto);
-        $query->execute();
-     }
+   public function inserir($id_user, $tipo_user, $texto, $url_noti, $icone)
+   {
+      $query = $this->db->prepare("INSERT INTO notificacao (id_user, tipo_user, texto, url_noti, icone) values (:id_user, :tipo_user, :texto, :url_noti, :icone)");
+      $query->bindParam(":id_user", $id_user);
+      $query->bindParam(":tipo_user", $tipo_user);
+      $query->bindParam(":texto", $texto);
+      $query->bindParam(":url_noti", $url_noti);
+      $query->bindParam(":icone", $icone);
+      $query->execute();
+   }
 
 }
--- cliente/App/Controllers/notificacao.php
+++ cliente/App/Controllers/notificacao.php
@@ -8,12 +8,16 @@ use App\ControllerSeguro;
 class Notificacao extends ControllerSeguro
 {
 
-    public function notifica($id_user, $tipo_user){
-        $query =$this->db->prepare('SELECT * FROM notifica Where id_user=:id_user and tipo_user=:tipo_user');
-        $query->bindParam(":id_user", $id_user);
-        $query->bindParam(":tipo_user", $tipo_user);
-        $query->execute();
-     }
+   public function notifica()
+   {
+      $id_user = $_SESSION['id_cli'];
+      $tipo_user = "cli";
+      $query = $this->db->prepare('SELECT * FROM notificacao Where id_user=:id_user and tipo_user=:tipo_user and data_leitura is null ');
+      $query->bindParam(":id_user", $id_user);
+      $query->bindParam(":tipo_user", $tipo_user);
+      $query->execute();
+      $this->jsonResponse($query->fetchAll());
+   }
 
      public function inserir($id_user, $tipo_user, $texto, $url_noti, $icone){
         $query = $this->db->prepare("INSERT INTO notificacao (id_user, tipo_user, texto, url_noti, icone) values (:id_user, :tipo_user, :texto, :url_noti, :icone)");
