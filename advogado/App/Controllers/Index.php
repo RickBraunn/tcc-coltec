36a29e3dc7b816c0a2863be4e01faa19b6aa68f1
Ricardo Vergutz <ricardovergutz@gmail.com>
2020-11-16 16:41:28 -0300

    correção pg inicial

--- advogado/App/Controllers/Index.php
+++ advogado/App/Controllers/Index.php
@@ -17,19 +17,10 @@ class Index Extends ControllerSeguro
 //        $resultado = $db->query("SHOW TABLES");
 //        $tabela = $resultado->fetchAll();
 
-
-        echo $this->template->twig->render('inicial.html.twig');
-
-    }
+        header('location: /advogado');
 
 
-    public function edit($id){
-        if ($id==''){
-            $this->errorNotFound('objeto não encontrado');
-        }
-
-        $a = [ 'aaa' => '54654'];
-        echo $this->jsonResponse($a);
 
     }
+
 }
